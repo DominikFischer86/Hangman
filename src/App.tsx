bf5c5f786b3f498f30e6495adf9a0caa21b4f3c4
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-12-05 23:02:27 +0100

    feat: Save highscore to localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 
 import { words } from "./data/wordlistGerman"
+import { useLocalstorage } from "./hooks/useLocalstorage"
 import Word from "./components/Word"
 import Letters from "./components/Letters"
 import StartGame from "./components/StartGame"
@@ -21,10 +22,13 @@ const initialState = {
 }
 
 const App = () => {
-  const [guessedLetters, setGuessedLetters] = useState<string[]>(initialState.guessedLetters)
+  const [guessedLetters, setGuessedLetters] = useState<string[]>(
+    initialState.guessedLetters
+  )
   const [word, setWord] = useState<string>(initialState.word)
   const [tries, setTries] = useState<number>(initialState.tries)
   const [winStreak, setWinStreak] = useState<number>(0)
+  const [highscore, setHighscore] = useLocalstorage("highscore", 0)
   const [gamestart, setGamestart] = useState<boolean>(initialState.gameStart)
   const [gameOver, setGameOver] = useState<boolean>(initialState.gameOver)
   const [victory, setVictory] = useState<boolean>(initialState.victory)
@@ -34,10 +38,14 @@ const App = () => {
     if (gamestart) setVictory(checkVictoryCondition(word, guessedLetters))
   }, [tries, word, guessedLetters])
 
+  useEffect(() => {
+    if (winStreak > highscore) setHighscore(winStreak)
+  }, [winStreak])
+
   const getKey = (letter: string) => {
     setGuessedLetters((currentLetters: string[]) => [...currentLetters, letter])
     if (word.includes(letter)) return
-    else return setTries(tries => tries-1)
+    else return setTries(tries => tries - 1)
   }
 
   const getRandomWord = (words: string[]): string => {
@@ -52,7 +60,9 @@ const App = () => {
   }
 
   const handleNewGame = (result: string): void => {
-    result === "victory" ? setWinStreak(winStreak => winStreak + 1) : setWinStreak(0)
+    result === "victory"
+      ? setWinStreak(winStreak => winStreak + 1)
+      : setWinStreak(0)
     setGuessedLetters(initialState.guessedLetters)
     setWord(initialState.word)
     setTries(initialState.tries)
@@ -63,37 +73,32 @@ const App = () => {
 
   const checkVictoryCondition = (word: string, guessedLetters: string[]) => {
     const wordArray: string[] = [...new Set(word.split(""))]
-    const filteredArray: string[] = wordArray.filter(value => guessedLetters.includes(value))
+    const filteredArray: string[] = wordArray.filter(value =>
+      guessedLetters.includes(value)
+    )
     return wordArray.length === filteredArray.length
   }
 
   return (
     <div className="board">
-      {!gamestart && <StartGame handleStart={handleStart} />}
-      {gamestart &&
+      {!gamestart && <StartGame handleStart={handleStart} winStreak={winStreak} highscore={highscore} />}
+      {gamestart && (
         <div className="main">
-          {winStreak >= 2 && <WinStreakCounter winStreak={winStreak} />}
-          <Gallows
-            tries={tries}
-            gameOver={gameOver}
-            victory={victory}
-          />
+          <WinStreakCounter winStreak={winStreak} highscore={highscore} />
+          <Gallows tries={tries} gameOver={gameOver} victory={victory} />
           <Word
             word={word}
             guessedLetters={guessedLetters}
             gameOver={gameOver}
             victory={victory}
           />
-          {!gameOver && !victory &&
-            <Letters
-              getKey={getKey}
-              guessedLetters={guessedLetters}
-            />
-          }
+          {!gameOver && !victory && (
+            <Letters getKey={getKey} guessedLetters={guessedLetters} />
+          )}
           {gameOver && <GameOver handleNewGame={handleNewGame} />}
           {victory && <Victory handleNewGame={handleNewGame} />}
         </div>
-      }
+      )}
     </div>
   )
 }
