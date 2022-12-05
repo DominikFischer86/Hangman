e5f586f9f9f962f1603745220b78d433a672f7c2
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-12-05 23:02:50 +0100

    feat: Create saved hangman

--- src/components/Gallows.tsx
+++ src/components/Gallows.tsx
@@ -1,31 +1,47 @@
 type GallowsProps = {
-    tries: number
-    gameOver: boolean
-    victory: boolean
+  tries: number
+  gameOver: boolean
+  victory: boolean
 }
 
-const Gallows = ({ tries, gameOver, victory } : GallowsProps): JSX.Element => {
-    const bodyParts: string[] = gameOver
-        ? ["head", "body", "left_arm", "right_arm", "left_leg", "right_leg", "ropeGO"]
-        : ["head", "body", "left_arm", "right_arm", "left_leg", "right_leg"]
-    const gallowsParts: string[] = gameOver
-        ? ["base", "beam", "top"]
-        : ["base", "beam", "top", "rope"]
+const Gallows = ({ tries, gameOver, victory }: GallowsProps): JSX.Element => {
+  const bodyParts: string[] = gameOver
+    ? [
+        "head",
+        "body",
+        "left_arm",
+        "right_arm",
+        "left_leg",
+        "right_leg",
+        "ropeGO",
+      ]
+    : ["head", "body", "left_arm", "right_arm", "left_leg", "right_leg"]
+  const gallowsParts: string[] = gameOver
+    ? ["base", "beam", "top"]
+    : ["base", "beam", "top", "rope"]
 
-    const getGallows = () => gallowsParts.map(part =>
-        (<div key={part} className={part}></div>))
+  const getGallows = () =>
+    gallowsParts.map(part => <div key={part} className={part}></div>)
 
-    const getHangman = () => bodyParts.reverse().slice(tries).map(part =>
-        (<div key={part} className={`${part} tries-${tries}`}></div>))
+  const getHangman = () =>
+    bodyParts
+      .reverse()
+      .slice(tries)
+      .map(part => <div key={part} className={`${part} tries-${tries}`}></div>)
 
-    return (
-        <div className={victory ? "gallows victory": "gallows"}>
-            {getGallows()}
-            <div className={gameOver ? "hangman swinging" :"hangman"}>
-                {getHangman()}
-            </div>
-        </div>
-    )
+    const getSavedHangman = () =>
+    bodyParts
+      .reverse()
+      .map(part => <div key={part} className={`${part}`}></div>)
+
+  return (
+    <div className={victory ? "gallows victory" : "gallows"}>
+      {getGallows()}
+      <div className={gameOver ? "hangman swinging" : "hangman"}>
+        {victory ? getSavedHangman() : getHangman()}
+      </div>
+    </div>
+  )
 }
 
 export default Gallows
